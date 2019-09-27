9a1af0058924854cbd1e57bdb6786372c50fd656
Nicole Nguyen <nnguy099@ucr.edu>
2019-09-27 13:14:40 -0700

    something weird

--- front_end/src/components/Home.js
+++ front_end/src/components/Home.js
@@ -79,140 +79,130 @@ class Home extends Component {
     window.location.assign('https://www.instagram.com/citrushack_ucr/')
   }
 
-<<<<<<< HEAD
+  <div className="heroStyling">
+  <Navbar />
+  <div className="mainHeaderContainer">
+    <h1 className="mainTitle">CUTIE HACK</h1>
+    <div data-aos="fade-right" data-aos-delay="200" className="mainUnderline"></div>
+    <h2 className="mainSubTitle">NOVEMBER 9, 2019</h2>
+  </div>
+</div>
+        </Animated >
+  <ScrollableAnchor id={'section2'}>
+    <div className="section2">
+      <div className="s1">
+        <img data-aos="fade-up" className="section2img" src={cutieIcon}></img>
+      </div>
+      <div className="s2">
+        <h1 data-aos="fade-up" className="sec2Title">ABOUT CUTIE HACK</h1>
+        <p data-aos="fade-up" className="sec2Text">Cutie Hack is a beginner-friendly, 12-hour hackathon. Hosted at UC Riverside, Cutie Hack is designed to help new hackers get used to the time crunch of a hackathon environment. <br></br> <br></br>Cutie Hack invites collegiate students to UC Riverside to collaborate and innovate. Throughout the 12 hours, participants work in teams on a project, attend workshops to learn about new technologies, and network with industry partners.</p>
+      </div>
+    </div>
+  </ScrollableAnchor>
+  <ScrollableAnchor id={'section3'}>
+    <div className="section3">
+      <div style={{ width: '100%', textAlign: 'center' }}>
+        <h1 className="sec3Title">FAQs</h1>
+      </div>
+      <Container fluid className="noPadding">
+        <Row className="noMargin">
+          <Col className="noPadding">
+            <div className="faqBubble" style={{ float: 'right' }}>
+              <div>
+                <Arrow title={'What is a hackathon?'} body={"A hackathon is a place where you and hundreds of other people learn, build, and create new technologies over the course of one weekend! Hackathons let you try learning a new skill, commit to that crazy idea you've never had time for, or make new friendships and strengthen old ones."} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'Who can attend?'} body={'We welcome all undergraduate UCR students! We do, however, encourage novice and first time hackers for this event.'} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'Is there free food?'} body={'Yes! Meals, refreshments, and snacks will be provided throughout the event.'} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'Do I need a team?'} body={'Teams are not required, but are recommended. Teams are capped at 4 members and you will have an opportunity to form teams at the start of the event as well.'} />
+              </div>
 
-  render() {
-    return (
-=======
-  render(){
-    return(
->>>>>>> f9b43a55a80b122d3ac6393b24e994466fd16579
-      <div>
-        <Animated animationIn="fadeIn" isVisible={true}>
-          <div className="heroStyling">
-            <Navbar />
-            <div className="mainHeaderContainer">
-              <h1 className="mainTitle">CUTIE HACK</h1>
-              <div data-aos="fade-right" data-aos-delay="200" className="mainUnderline"></div>
-              <h2 className="mainSubTitle">NOVEMBER 9, 2019</h2>
             </div>
-          </div>
-        </Animated>
-        <ScrollableAnchor id={'section2'}>
-          <div className="section2">
-            <div className="s1">
-              <img data-aos="fade-up" className="section2img" src={cutieIcon}></img>
-            </div>
-            <div className="s2">
-              <h1 data-aos="fade-up" className="sec2Title">ABOUT CUTIE HACK</h1>
-              <p data-aos="fade-up" className="sec2Text">Cutie Hack is a beginner-friendly, 12-hour hackathon. Hosted at UC Riverside, Cutie Hack is designed to help new hackers get used to the time crunch of a hackathon environment. <br></br> <br></br>Cutie Hack invites collegiate students to UC Riverside to collaborate and innovate. Throughout the 12 hours, participants work in teams on a project, attend workshops to learn about new technologies, and network with industry partners.</p>
-            </div>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor id={'section3'}>
-          <div className="section3">
-            <div style={{ width: '100%', textAlign: 'center' }}>
-              <h1 className="sec3Title">FAQs</h1>
-            </div>
-            <Container fluid className="noPadding">
-              <Row className="noMargin">
-                <Col className="noPadding">
-                  <div className="faqBubble" style={{ float: 'right' }}>
-                    <div>
-                      <Arrow title={'What is a hackathon?'} body={"A hackathon is a place where you and hundreds of other people learn, build, and create new technologies over the course of one weekend! Hackathons let you try learning a new skill, commit to that crazy idea you've never had time for, or make new friendships and strengthen old ones."} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'Who can attend?'} body={'We welcome all undergraduate UCR students! We do, however, encourage novice and first time hackers for this event.'} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'Is there free food?'} body={'Yes! Meals, refreshments, and snacks will be provided throughout the event.'} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'Do I need a team?'} body={'Teams are not required, but are recommended. Teams are capped at 4 members and you will have an opportunity to form teams at the start of the event as well.'} />
-                    </div>
-
-                  </div>
-                </Col>
-                <Col className="noPadding">
-                  <div className="faqBubble">
-                    <div>
-                      <Arrow title={'Is Cutie Hack free?'} body={'Absolutely! There is no cost to attend, but do bring your own hacking devices!'} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'What should I bring?'} body={'Student IDs are required. Consider bringing a hacking machine, headphones, and a pillow. Feel free to bring your own parts as well but note that soldering is not allowed at CutieHack.'} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'What if I still have a question?'} body={'Email us at citrushack@gmail.com. We love answering  questions!'} />
-                    </div>
-                    <div className="secondaryFaq">
-                      <Arrow title={'What if I do not know how to code?'} body={"CutieHack is a beginner friendly hackathon and is open to all skill levels. During the event, take the time to checkout workshops and collaborate with others."} />
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </ScrollableAnchor>
-        <div className="stats">
-          <div style={{ display: 'flex' }}>
-            <div className="statsContainer">
-              <span data-aos="fade-down" data-aos-duration="200" className="statsHeader">12</span>
-              <br></br>
-              <span className="statsSubHeader" style={{ paddingLeft: '10px' }}>HOURS</span>
-            </div>
-            <div className="statsContainer">
-              <span data-aos="fade-down" data-aos-duration="200" data-aos-delay="200" className="statsHeader">300</span>
-              <br></br>
-              <span className="statsSubHeader">HACKERS</span>
-            </div>
-            <div className="statsContainer">
-              <span data-aos="fade-down" data-aos-duration="200" data-aos-delay="400" className="statsHeader">75</span>
-              <br></br>
-              <span className="statsSubHeader">PROJECTS</span>
-            </div>
-          </div>
-        </div>
-        <ScrollableAnchor id={'section4'}>
-          <div className="section4">
-            <div style={{ width: '100%', textAlign: 'center' }}>
-              <h1 className="sec4Title">SPECIAL THANKS</h1>
-              <Container style={{ marginTop: '60px' }} fluid className="noPadding">
-                <Row className="noMargin">
-                  <Col className="noPadding">
-                    <a href="https://acmucr.org/">
-                      <img className="acm" src={acm}></img>
-                    </a>
-                  </Col>
-                  <Col className="noPadding">
-                    <a href="https://ieee.ee.ucr.edu/">
-                      <img className="ieee" src={ieee}></img>
-                    </a>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-            <img className="homeFooter" src={cutieFooter}></img>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor id={'section5'}>
-          <div className="footer">
-            <div className="footerWrap">
-              <div style={{ margin: 'auto' }}>
-                <Icon onClick={this.handleFacebook} className="footerIcon" type="facebook" />
-                <Icon onClick={this.handleMail} className="footerIcon" type="mail" />
-                <Icon onClick={this.handleTwitter} className="footerIcon" type="twitter-square" theme="filled" />
-                <Icon onClick={this.handleInstagram} className="footerIcon" type="instagram" />
+          </Col>
+          <Col className="noPadding">
+            <div className="faqBubble">
+              <div>
+                <Arrow title={'Is Cutie Hack free?'} body={'Absolutely! There is no cost to attend, but do bring your own hacking devices!'} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'What should I bring?'} body={'Student IDs are required. Consider bringing a hacking machine, headphones, and a pillow. Feel free to bring your own parts as well but note that soldering is not allowed at CutieHack.'} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'What if I still have a question?'} body={'Email us at citrushack@gmail.com. We love answering  questions!'} />
+              </div>
+              <div className="secondaryFaq">
+                <Arrow title={'What if I do not know how to code?'} body={"CutieHack is a beginner friendly hackathon and is open to all skill levels. During the event, take the time to checkout workshops and collaborate with others."} />
               </div>
             </div>
-            <div style={{ textAlign: 'center' }}>
-              <p className="footerText">Made with ♥ in Riverside, CA</p>
-            </div>
-            <div style={{ paddingBottom: '0.5%', textAlign: 'center' }}>
-              <p className="footerText">© 2019 Cutie Hack</p>
-            </div>
-          </div>
-        </ScrollableAnchor>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </ScrollableAnchor>
+  <div className="stats">
+    <div style={{ display: 'flex' }}>
+      <div className="statsContainer">
+        <span data-aos="fade-down" data-aos-duration="200" className="statsHeader">12</span>
+        <br></br>
+        <span className="statsSubHeader" style={{ paddingLeft: '10px' }}>HOURS</span>
       </div>
+      <div className="statsContainer">
+        <span data-aos="fade-down" data-aos-duration="200" data-aos-delay="200" className="statsHeader">300</span>
+        <br></br>
+        <span className="statsSubHeader">HACKERS</span>
+      </div>
+      <div className="statsContainer">
+        <span data-aos="fade-down" data-aos-duration="200" data-aos-delay="400" className="statsHeader">75</span>
+        <br></br>
+        <span className="statsSubHeader">PROJECTS</span>
+      </div>
+    </div>
+  </div>
+  <ScrollableAnchor id={'section4'}>
+    <div className="section4">
+      <div style={{ width: '100%', textAlign: 'center' }}>
+        <h1 className="sec4Title">SPECIAL THANKS</h1>
+        <Container style={{ marginTop: '60px' }} fluid className="noPadding">
+          <Row className="noMargin">
+            <Col className="noPadding">
+              <a href="https://acmucr.org/">
+                <img className="acm" src={acm}></img>
+              </a>
+            </Col>
+            <Col className="noPadding">
+              <a href="https://ieee.ee.ucr.edu/">
+                <img className="ieee" src={ieee}></img>
+              </a>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <img className="homeFooter" src={cutieFooter}></img>
+    </div>
+  </ScrollableAnchor>
+  <ScrollableAnchor id={'section5'}>
+    <div className="footer">
+      <div className="footerWrap">
+        <div style={{ margin: 'auto' }}>
+          <Icon onClick={this.handleFacebook} className="footerIcon" type="facebook" />
+          <Icon onClick={this.handleMail} className="footerIcon" type="mail" />
+          <Icon onClick={this.handleTwitter} className="footerIcon" type="twitter-square" theme="filled" />
+          <Icon onClick={this.handleInstagram} className="footerIcon" type="instagram" />
+        </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <p className="footerText">Made with ♥ in Riverside, CA</p>
+      </div>
+      <div style={{ paddingBottom: '0.5%', textAlign: 'center' }}>
+        <p className="footerText">© 2019 Cutie Hack</p>
+      </div>
+    </div>
+  </ScrollableAnchor>
+      </div >
     )
   }
 }
